e193545a62be96bd94b9a7aeea47d06c0bc67548
lzkmylz <lzkmylz@163.com>
2019-07-25 14:22:36 +0800

    add member_info and prepare to read constantpool

--- ch03/classfile/class_file.go
+++ ch03/classfile/class_file.go
@@ -9,6 +9,7 @@ type ClassFile struct {
 	constantPool ConstantPool
 	accessFlags uint16
 	thisClass uint16
+	superClass uint16
 	interfaces []uint16
 	fields []*MemberInfo
 	methods []*MemberInfo
@@ -32,49 +33,77 @@ func Parse(ClassData []byte) (cf *ClassFile, err error) {
 }
 
 func (self *ClassFile) read(reader *ClassReader) {
-
+	self.readAndCheckMagic(reader)
+	self.readAndCheckVersion(reader)
+	self.constantPool = self.readConstantPool(reader)
+	self.accessFlags = reader.readUint16()
+	self.thisClass = reader.readUint16()
+	self.interfaces = reader.readUint16()
+	self.fields = readMembers(reader, self.constantPool)
+	self.methods = readMembers(reader, self.constantPool)
+	self.attributes = readAttributes(reader, self.constantPool)
 }
 
 func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
-
+	magic := reader.readUint32()
+	if magic != 0xCAFEBABE {
+		panic("java.lang.ClassFormatError: magic!")
+	}
 }
 
 func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
-
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+	panic("java.lang.UnsupportedClassVersion")
 }
 
 func (self *ClassFile) MinorVersion() uint16 { // getter
-
+	return self.minorVersion
 }
 
 func (self *ClassFile) MajorVersion() uint16 { // getter
-
+	return self.majorVersion
 }
 
 func (self *ClassFile) ConstantPool() ConstantPool { // getter
-
+	return self.constantPool
 }
 
 func (self *ClassFile) AccessFlags() uint16 { // getter
-
+	return self.accessFlags
 }
 
 func (self *ClassFile) Fields() []*MemberInfo { // getter
-
+	return self.fields
 }
 
 func (self *ClassFile) Methods() []*MemberInfo { // getter
-
+	return self.methods
 }
 
 func (self *ClassFile) ClassName() string { // getter
-
+	return self.constantPool.getClassName(self.thisClass)
 }
 
 func (self *ClassFile) SuperClassName() string { // getter
-
+	if self.superClass > 0 {
+		return self.constantPool.getClassName(self.superClass)
+	}
+	return "" // 只有java.lang.Object没有super class
 }
 
 func (self *ClassFile) InterfaceNames() []string { // getter
-
+	interfaceNames := make([]string, len(self.interfaces))
+	for i, cpIndex := range self.interfaces {
+		interfaceNames[i] = self.constantPool.getClassName(cpIndex)
+	}
+	return interfaceNames
 }
